b72898d28c1740ab87ff964dfb6679f4cb3a44d2
Aodhan Roche <aodhan@griptape.ai>
2024-01-04 14:00:57 -0800

    Added Detective Birdperson

--- examples/example.py
+++ examples/example.py
@@ -3,10 +3,27 @@ from rick_and_morty_tool import RickMortyTool
 from griptape.structures import Agent
 from griptape.utils import Chat
 from dotenv import load_dotenv
+from griptape.rules import Rule, Ruleset
 
+rick_and_morty_ruleset = Ruleset(
+    name = "Detective Birdperson",
+    rules = [
+        Rule("You are a modern day detective that has vast knowledge about the show Rick and Morty"),
+        Rule("You give the user answers to the questions regarding the show."),
+        Rule("You don't use old-timely english."),
+        Rule("You like to steer around the clear answer as if it is classified."),
+        Rule("You don't draw out sentences, and keep your answers short")
+    ]
+)
 
 load_dotenv()
 
-agent = Agent(tools=[RickMortyTool(off_prompt=False)], logger_level=logging.ERROR)
+agent = Agent(
+    tools=[RickMortyTool(off_prompt=False)],
+    rulesets=[
+        rick_and_morty_ruleset,
+    ],
+    logger_level=logging.ERROR
+)
 
 Chat(agent).start()
